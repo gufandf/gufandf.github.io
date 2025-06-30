be46fa6d572a090b56dea94a9369ca4ac9dcb487
孤帆Gufandf <gufandf@qq.com>
2025-07-01 00:36:40 +0800

    MBS

--- top.py
+++ top.py
@@ -1,5 +1,6 @@
 import markdown
 import shutil, os, re, time
+import logging
 
 templatesRoot = "./site/templates/"
 modelsRoot = "./models/"
@@ -7,6 +8,7 @@ postsRoot = "./site/p/"
 siteRoot = "./site/"
 buildRoot = "./docs/"
 modsRoot = "./site/mods/"
+
 
 def coPath(path1: str, path2: str):
     path1 = path1.replace("./", "")
@@ -38,91 +40,94 @@ def walkPath(Path):
     return files
 
 def buildFile(filePath):
-    pass
+    fileType = filePath.split(".")[-1]
+    target_filename = "./docs/"+"/".join(filePath.split("/")[2:]).split(".")[0]+".html"
+    if fileType == "html":
+        f = open(filePath, "r", encoding="UTF-8")
+        file = f.read()
+        f.close()
+        # 如果使用模板
+        if len(re.findall('<template src=".+?">', file)) == 0:
+            return
+        templateName = re.findall('<template src=".+?">', file)[0][15:-2]
+        childs = re.findall('<child id="(.+?)">([^*]*?)</child>', file)
+        # logging.info("childs: "+json.dumps(childs))
+
+        # 填充模版
+        html = templates[templateName]
+        for child in childs:
+            # logging.info(child)
+            html = html.replace(f'{{{{{child[0]}}}}}', child[1])
+        for child in re.findall("{{(.*?)}}", html):# 清除未填充的模版
+            html = html.replace(f'{{{{{child}}}}}', "") 
+        # 替换模组
+        for mod in mods:
+            html = html.replace(f"<mod src=\"{mod}\">",mods[mod])
+        f = open(target_filename, "w", encoding="UTF-8")
+        f.write(html)
+        f.close()
+    elif fileType == "md":
+        f = open(filePath, "r", encoding="UTF-8")
+        file = f.read()
+        f.close()
+        childs = {}
+        for i in re.findall("<!-- (.*): (.*) -->", file):
+            childs[i[0]] = i[1].strip()
+        childs["content"] = markdown.markdown(file,extensions=['markdown.extensions.toc','markdown.extensions.fenced_code','markdown.extensions.tables'])
+        # 填充模版
+        html = templates["base"]
+        for child in childs:
+            html = html.replace(f'{{{{{child}}}}}', childs[child])
+        for child in re.findall("{{(.*?)}}", html):# 清除未填充的模版
+            html = html.replace(f'{{{{{child}}}}}', "") 
+        # logging.info(re.findall("{{(.*?)}}", html)) # 清除未填充的模版
+        # 替换模组
+        for mod in mods:
+            html = html.replace(f"<mod src=\"{mod}\">",mods[mod])
+        f = open(target_filename, "w", encoding="UTF-8")
+        f.write(html)
+        f.close()
 
 
 def build():
     shutil.rmtree(buildRoot)
     shutil.copytree(siteRoot, buildRoot, True)
 
-    templates = readFileIn(templatesRoot)
-    mods = readFileIn(modsRoot)
     # models = readFileIn(modelsRoot)
 
-    # print("[build] building...")
     for fileOrg in os.walk(buildRoot):
-        # print(f"[build] build {fileOrg}...")
+        # logging.info(f"[build] build {fileOrg}...")
         # fileOrg ('./build', ['p'], ['index copy.html', 'index.html'])
         for fileName in fileOrg[2]:
-            fileType = fileName.split(".")[-1]
-            
-            if fileType == "html":
-                f = open(coPath(fileOrg[0], fileName), "r", encoding="UTF-8")
-                file = f.read()
-                f.close()
-                # 如果使用模板
-                if len(re.findall('<template src=".+?">', file)) == 0:
-                    break
-                templateName = re.findall('<template src=".+?">', file)[0][15:-2]
-                childs = re.findall('<child id="(.+?)">([^*]*?)</child>', file)
-                # print("childs: "+json.dumps(childs))
+            buildFile(coPath(fileOrg[0], fileName))
+    logging.info(nowTime+"构建完成")
 
-                # 填充模版
-                html = templates[templateName]
-                for child in childs:
-                    # print(child)
-                    html = html.replace(f'{{{{{child[0]}}}}}', child[1])
-                for child in re.findall("{{(.*?)}}", html):# 清除未填充的模版
-                    html = html.replace(f'{{{{{child}}}}}', "") 
-                # 替换模组
-                for mod in mods:
-                    html = html.replace(f"<mod src=\"{mod}\">",mods[mod])
-                f = open(coPath(fileOrg[0], fileName), "w", encoding="UTF-8")
-                f.write(html)
-                f.close()
-            elif fileType == "md":
-                f = open(coPath(fileOrg[0], fileName), "r", encoding="UTF-8")
-                file = f.read()
-                f.close()
-                childs = {}
-                for i in re.findall("<!-- (.*): (.*) -->", file):
-                    childs[i[0]] = i[1].strip()
-                childs["content"] = markdown.markdown(file,extensions=['markdown.extensions.toc','markdown.extensions.fenced_code','markdown.extensions.tables'])
-                # 填充模版
-                html = templates["base"]
-                for child in childs:
-                    html = html.replace(f'{{{{{child}}}}}', childs[child])
-                for child in re.findall("{{(.*?)}}", html):# 清除未填充的模版
-                    html = html.replace(f'{{{{{child}}}}}', "") 
-                # print(re.findall("{{(.*?)}}", html)) # 清除未填充的模版
-                # 替换模组
-                for mod in mods:
-                    html = html.replace(f"<mod src=\"{mod}\">",mods[mod])
-                f = open(coPath(fileOrg[0], fileName)[:-3]+".html", "w", encoding="UTF-8")
-                f.write(html)
-                f.close()
-    print(nowTime+" [Build] 构建完成")
 
-watchList = {}
-nowTime = ""
-build()
-while True:
-    nowTime = f"{str(time.localtime().tm_hour).ljust(2,'0')}:{str(time.localtime().tm_min).ljust(2,'0')}:{str(time.localtime().tm_sec).ljust(2,'0')}"
-    filePaths = walkPath(siteRoot)
-    for filePath in filePaths:
-        try:
-            if watchList[filePath] != os.path.getmtime(filePath):
-                print(nowTime+" [Change] "+filePath)
-                watchList[filePath] = os.path.getmtime(filePath)
+templates = readFileIn(templatesRoot)
+mods = readFileIn(modsRoot)
+
+if __name__ == "__main__":
+    watchList = {}
+    nowTime = ""
+    build()
+    while True:
+        nowTime = f"{str(time.localtime().tm_hour).ljust(2,'0')}:{str(time.localtime().tm_min).ljust(2,'0')}:{str(time.localtime().tm_sec).ljust(2,'0')}"
+        filePaths = walkPath(siteRoot)
+        for filePath in filePaths:
+            try:
+                if watchList[filePath] != os.path.getmtime(filePath):
+                    logging.info(nowTime+" [Change] "+filePath)
+                    watchList[filePath] = os.path.getmtime(filePath)
+                    buildFile(filePath)
+            except FileNotFoundError:
+                logging.info(nowTime+"[Change]"+filePath)
                 build()
-        except FileNotFoundError:
-            print(nowTime+" [Change] "+filePath)
-            build()
-        except KeyError:
-            watchList[filePath] = os.path.getmtime(filePath)
-        except PermissionError:
-            print(nowTime+" [Error] 文件被占用: "+filePath)
-        except:
-            print(nowTime+" [Error] 未知错误: "+filePath)
-    time.sleep(3)
-# build()
+            except KeyError:
+                watchList[filePath] = os.path.getmtime(filePath)
+            except PermissionError:
+                # logging.warning(nowTime+"文件被占用: "+filePath)
+                pass
+            except:
+                logging.warning(nowTime+"未知错误: "+filePath)
+        time.sleep(3)
+    # build()
